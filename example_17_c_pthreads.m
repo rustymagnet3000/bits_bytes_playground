1d4b0f286ee00a9313ababe237854f65f7cf213b
Rusty Robot <rustyrobot@robot.com>
2019-05-22 10:25:06 +0100

    added gcd, nsoperations and pthread examples

--- example_17_c_pthreads.m
+++ example_17_c_pthreads.m
@@ -1,11 +1,14 @@
 #include <pthread.h>
 #include <stdio.h>
-
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
 /*********************************************************************************
  Start two background threads.
  Both print a message to logs.
- Goal 1: use pthread_join to main thread waits for background threads to finish
+ Goal: pause a single thread why the other thread continues
  *********************************************************************************/
+const unsigned int microseconds = 10;
 
 typedef struct{
     int count;
@@ -14,34 +17,36 @@ typedef struct{
 
 void *hello_world(void *voidptr) {
     uint64_t tid;
+
     assert(pthread_threadid_np(NULL, &tid)== 0);
     printf("Thread ID: dec:%llu hex: %#08x\n", tid, (unsigned int) tid);
-    Chomper *chomper = (Chomper *)voidptr;  // help the compiler understand how tochange the void pointer to the actual data structure
-    
+    Chomper *chomper = (Chomper *)voidptr;  // help the compiler map the void pointer to the actual data structure
+
     for (int i = 0; i < chomper->count; i++) {
-        sleep(1);
+        usleep(microseconds);
         printf("%s: %d\n", chomper->message, i);
     }
     return NULL;
 }
 
 int main() {
-    @autoreleasepool {
-      
-        pthread_t myThread1 = NULL, myThread2 = NULL;
-        
-        Chomper *shark = malloc(sizeof(*shark));
-        shark->count = 5;
-        shark->message = "hello";
 
-        Chomper *jellyfish = malloc(sizeof(*jellyfish));
-        jellyfish->count = 20;
-        jellyfish->message = "goodbye";
+    pthread_t myThread1 = NULL, myThread2 = NULL;
 
-        assert(pthread_create(&myThread1, NULL, hello_world, (void *) shark) == 0);
-        assert(pthread_create(&myThread2, NULL, hello_world, (void *) jellyfish) == 0);
-        assert(pthread_join(myThread1, NULL) == 0);
-        assert(pthread_join(myThread2, NULL) == 0);
-    }
+    Chomper *shark = malloc(sizeof(*shark));
+    shark->count = 5;
+    shark->message = "shark";
+
+    Chomper *jellyfish = malloc(sizeof(*jellyfish));
+    jellyfish->count = 5;
+    jellyfish->message = "jelly";
+
+    assert(pthread_create(&myThread1, NULL, hello_world, (void *) shark) == 0);
+    assert(pthread_create(&myThread2, NULL, hello_world, (void *) jellyfish) == 0);
+    assert(pthread_join(myThread1, NULL) == 0);
+    assert(pthread_join(myThread2, NULL) == 0);
+
+    free(shark);
+    free(jellyfish);
     return 0;
 }
---
+++ example_18_objc_nsoperation.m
@@ -0,0 +1,87 @@
+// https://nshipster.com/nsoperation/
+// https://www.appcoda.com/understanding-key-value-observing-coding/
+
+#ifdef DEBUG
+#define NSLog(FORMAT, ...) fprintf(stderr,"%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+#else
+#define NSLog(...) {}
+#endif
+
+#import <Foundation/Foundation.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <pthread.h>
+
+@interface YDGCDClass : NSOperation
+
+- (void) start;
+- (void) finished;
+@property BOOL isExecuting, isFinished;
+
+@end
+
+@implementation YDGCDClass
+
+// Note, you don't need to @synthesize isExecuting, isFinished!
+- (void) setNotification {
+    NSLog(@"[+] isFinished() Observer setting");
+
+    [[NSNotificationCenter defaultCenter] addObserverForName:@"isFinished" object:nil queue:nil usingBlock:^(NSNotification *note)
+     {
+         NSLog(@"[+] %@ COMPLETED\n", note.name);
+     }];
+}
+
+- (void)start
+{
+    self.isExecuting = YES;
+    self.isFinished = NO;
+    NSLog(@"[+] [YDGCDClass start] on Main thread? %@", [NSThread isMainThread] ? @"Yes" : @"No");
+    NSLog(@"[+] [YDGCDClass start] isExecuting? %@", self.isExecuting ? @"Yes" : @"No");
+    uint64_t tid;
+    assert(pthread_threadid_np(NULL, &tid)== 0);
+    NSLog(@"[+] Thread ID: %#08x", (unsigned int) tid);
+
+    for(int i = 0; i < 2; i++)
+    {
+        sleep(1);
+    }
+    [self finished];
+}
+
+- (void)finished
+{
+    self.isExecuting = NO;
+    self.isFinished = YES;
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"isFinished" object:self userInfo:nil];
+}
+
+@end
+
+
+
+int main() {
+    @autoreleasepool {
+
+        NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+        YDGCDClass *operation = [[YDGCDClass alloc] init];
+
+        operation.queuePriority = NSOperationQueuePriorityLow;
+        operation.qualityOfService = NSOperationQualityOfServiceBackground;
+
+        [operation setNotification];
+        [queue addOperation:operation];
+
+        [queue waitUntilAllOperationsAreFinished];      // stop main thread from finishing
+    }
+
+    return 0;
+}
+
+/*
+[+] [YDGCDClass start] on Main thread? No
+[+] [YDGCDClass start] isExecuting? Yes
+[+] Thread ID: 0x0d61b5
+[+] isFinished COMPLETED
+*/
