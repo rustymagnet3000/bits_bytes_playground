2cba37a5b232f2626aa833e6a0ea05c188a37325
Rusty Robot <rustyrobot@robot.com>
2019-10-25 07:57:09 +0100

    Added NSURL Swizzle, as it was tidier than a lot of the other Swizzle

--- example_23_objc_runtime_create_class.m
+++ example_23_objc_runtime_create_class.m
@@ -1,10 +1,14 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
+// https://gist.github.com/mikeash/7603035
+// https://www.bignerdranch.com/blog/inside-the-bracket-part-7-runtime-machinations/
+// http://www.alejandrosegovia.net/2012/08/06/dynamic-method-injection-in-objective-c/
+// http://labs.distriqt.com/post/846
+// https://github.com/hsnamr/Create-ObjC-Class-At-Runtime
+
 /* To add a method and ivar at run-time sounds simple but Automatic Reference Counting (ARC) stopped me for a long time. The below code won't compile with ARC on.
 
-https://www.bignerdranch.com/blog/inside-the-bracket-part-7-runtime-machinations/
- https://gist.github.com/mikeash/7603035
 https://stackoverflow.com/questions/20582642/why-arc-forbids-calls-to-undeclared-methods/20582863#20582863
 */
 
