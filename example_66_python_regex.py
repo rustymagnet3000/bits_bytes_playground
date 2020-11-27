6b5ef4753d407ad59fec4250bdd624fc2ab349dd
rustymagnet3000 <rustymagnet3000@gmail.com>
2020-11-27 14:57:17 +0000

    Refined the regex, with and argparse Python example

--- example_66_python_regex.py
+++ example_66_python_regex.py
@@ -3,49 +3,47 @@ import re
 import argparse
 
 
-def remove_wildcard(hostname):
-    if hostname[0:2] == "*.":
-        hostname = hostname[2:]
-        hostname = remove_wildcard(hostname)
-    return hostname
+class HostNameCleaner:
+    def __init__(self, orig_file):
+        self.raw_file = orig_file
+        self.hostnames = []
+        self.clean_hostnames()
 
+    def clean_hostnames(self):
+        """
+            Passed an Opened file
+            Check if each line is a valid Hostname
+            if line fails, return don't return that line
+        """
+        file_by_lines = self.raw_file.read().split("\n")
+        for line in file_by_lines:
+            if len(line) > 0 and line[0] != '#':
+                host_sanitize_l1 = HostNameCleaner.remove_wildcard(line)
+                host_sanitize_l2 = HostNameCleaner.is_valid_hostname(host_sanitize_l1)
+                if host_sanitize_l2 is not None:
+                    self.hostnames.append(host_sanitize_l2)
 
-def is_valid_hostname(hostname, regex: re.Pattern):
-    if len(hostname) > 255:
-        return None
-
-    x = re.findall(r'\.', hostname)
-    if len(x) == 0 or len(x) > 4:
-        return None
-
-    if len(re.findall(r'\s+', hostname)) > 0:    # remove hostnames with whitespace ( spaces / tabs )
-        return None
-
-    if all(regex.match(x) for x in hostname.split(".")) is not True:
+    @staticmethod
+    def remove_wildcard(hostname):
+        if hostname[0:2] == "*.":
+            hostname = hostname[2:]
+            hostname = HostNameCleaner.remove_wildcard(hostname)
         return hostname
 
-    return None
-
-
-def return_clean_hostnames(infile):
-    '''
-        Passed an Opened file
-        Check if each line can be passed into a web address.
-        If it succeeds, add it to url List
-        if line fails to be parsed by regex, throwaway and move to next line
-        :return: [hostnames]
-    '''
-    hostnames = []
-    hostname_regex = re.compile("(?!-)[A-Z\d-]{5,63}(?<!-)$", re.IGNORECASE)
-    file_by_lines = infile.read().split("\n")
-    for line in file_by_lines:
-        if len(line) > 0 and line[0] != '#':
-            host_sanitize_l1 = remove_wildcard(line)
-            host_sanitize_l2 = is_valid_hostname(host_sanitize_l1, hostname_regex)
-            if host_sanitize_l2 is not None:
-                hostnames.append(host_sanitize_l2)
-    for host in hostnames:
-        print(host)
+    @staticmethod
+    def is_valid_hostname(hostname):
+        hostname_regex = re.compile("(?!-)[A-Z\d-]{5,63}(?<!-)$", re.IGNORECASE)
+        num_periods = re.findall(r'\.', hostname)
+        num_non_alpha = re.findall(r'\W', hostname)
+        if len(hostname) > 255:
+            return None
+        if len(num_periods) == 0 or len(num_periods) > 4 or len(num_non_alpha) > len(num_periods):
+            return None
+        if len(re.findall(r'\s+', hostname)) > 0:    # remove hostnames with whitespace ( spaces / tabs )
+            return None
+        if all(hostname_regex.match(x) for x in hostname.split(".")) is not True:
+            return hostname
+        return None
 
 
 if __name__ == '__main__':
@@ -55,19 +53,22 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     with args.infile as file:
-        return_clean_hostnames(file)
+        cleaner = HostNameCleaner(file)
+        print(cleaner.hostnames)
 
 
-//input
-stackoverflow.com
-httpbin.org
-weirddtstst
-george. loves apples.
-*.google.com
-tab should  be  removed
+########### input ###########
+# no.commas,please
+# stackoverflow.com
+# httpbin.org
+# weirddtstst
+# GGorge. loves apples.
+# *.google.com
+# tab should  be  removed
+# ....%%%%%....@@£££
 
 
-// Result
-stackoverflow.com
-httpbin.org
-google.com
+########### Result ###########
+# stackoverflow.com
+# httpbin.org
+# google.com
