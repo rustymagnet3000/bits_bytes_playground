b1e318005896c25e207081e95113f147f09b664b
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-05-10 15:52:36 +0100

    Improved the parsing of for the GuardDuty response data

--- python/parse_dict_list_dict.py
+++ python/parse_dict_list_dict.py
@@ -1,16 +1,19 @@
+from dateutil.parser import parse
+
 # AWS Response
 # 1   Dict            ( a Dictionary that contains a Dict and List )
 # 2        \--->Dict['ResponseMetadata']
 # 3        \--->List['Findings']
 # 4            \--->Dict
-big_dict = {'ResponseMetadata': {'RequestId': 'xxxxxx-04e9-xxxx-xxx-xxxxxx',
+
+big_dict = {'ResponseMetadata': {'RequestId': 'xxxxxx-04e9-4594-98c9-xxxxxx',
                                  'HTTPStatusCode': 200,
                                  'HTTPHeaders': {'date': 'Mon, 10 May 2021 11:08:42 GMT',
                                                  'content-type': 'application/json',
                                                  'content-length': '7175',
                                                  'connection': 'keep-alive',
                                                  'x-amzn-requestid': 'xxxxxx-04e9-4594-98c9-xxxxxx',
-                                                 'x-amz-apigw-id': 'xxxxxxxxx=',
+                                                 'x-amz-apigw-id': 'fHAZLEG0joEFbOg=',
                                                  'x-amzn-trace-id': 'Root=1-xxxxxx;Sampled=0'
                                                  },
                                  'RetryAttempts': 0},
@@ -50,13 +53,19 @@ big_dict = {'ResponseMetadata': {'RequestId': 'xxxxxx-04e9-xxxx-xxx-xxxxxx',
                           'Severity': 5,
                           'Title': 'User AssumedRole : my-iam-role is anomalously invoking APIs commonly used in InitialAccess tactics.',
                           'Type': 'InitialAccess:IAMUser/AnomalousBehavior', 'UpdatedAt': '2021-05-06T13:15:21.786Z'}]}
+
+
+def validate_date_return_pretty(date_text):
+    cleaned_date = parse(date_text)
+    return f'{cleaned_date:%d-%m-%Y\t%H:%M%p}'
+
+
 print(f'[*]Top level:, {type(big_dict)}, \tlength: {len(big_dict)}')
 print(big_dict.keys())
 print(f'[*]Child 1:, {type(big_dict["Findings"])}, \tlength:{len(big_dict["Findings"])}')
 print(f'[*]Child 2:, {type(big_dict["ResponseMetadata"])}, \tlength:{len(big_dict["ResponseMetadata"])}')
 
 # Regular list print and list print with Sushi syntax
-print(big_dict['Findings'])
 print(big_dict['Findings'][::1])
 
 # 1 Get two child data structures from initial Dict
@@ -65,10 +74,19 @@ for key, value in big_dict.items():
     # 3 Only handle the list, as this has the useful info
     if isinstance(value, list):
 
-        # 4 Get the Dict out of the 3
-        dict_to_print = value[0]
+        # 4 get the Findings Dict out of the List
+        dict_of_findings = value[0]
 
-        # 4 print all items in dictionary
-        print(f'[*]Print each key-value pair from List')
-        for v in dict_to_print.items():
-            print("[*]", v)
+        if dict_of_findings['CreatedAt']:
+            print(f'[*]Date:           \t{validate_date_return_pretty(dict_of_findings["CreatedAt"])}')
+
+        # 5 get Resource Dict out of the Findings Dict (4)
+        if dict_of_findings['Resource']:
+            print(f'[*]Access Key:     \t{dict_of_findings["Resource"]["AccessKeyDetails"]["AccessKeyId"]}')
+            print(f'[*]ID:             \t{dict_of_findings["Resource"]["AccessKeyDetails"]["PrincipalId"]}')
+            print(f'[*]AWS username:   \t{dict_of_findings["Resource"]["AccessKeyDetails"]["UserName"]}')
+
+        if dict_of_findings['Description']:
+            print(f'\tDescription:{dict_of_findings["Description"]}')
+        if dict_of_findings['Region']:
+            print(f'\tRegion:{dict_of_findings["Region"]}')
