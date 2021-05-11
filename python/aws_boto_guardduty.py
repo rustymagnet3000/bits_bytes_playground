1e2eea647ed629d875545d734e0e2d4188a33e5c
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-05-11 10:05:51 +0100

    Pre-update of GuardDuty code parsing response code

--- python/aws_boto_guardduty.py
+++ python/aws_boto_guardduty.py
@@ -2,6 +2,7 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 
+
 # Set environment variables:
 #   AWS_PROFILE=default
 #   AWS_DEFAULT_REGION=......
@@ -11,27 +12,19 @@ def rm_list_guardduty_events():
     logging.info(f'[*]Starting to list GuardDuty events')
     try:
         gd = boto3.client('guardduty')
-        detectors = gd.list_detectors(
-            MaxResults=10,
-            NextToken='string'
-        )
+        detectors = gd.list_detectors(MaxResults=10, NextToken='string')
         no_of_detectors = len(detectors['DetectorIds'])
-        logging.info(f'[*]Detectors:{no_of_detectors}')
+        logging.info(f'[*]Detectors found:{no_of_detectors}')
         if no_of_detectors > 0:
             for d in detectors['DetectorIds']:
                 logging.info(f'Detector ID:\t{d}')
-                fc = {'Criterion': {'severity': {'Gte': 4}}}
+                fc = {'Criterion': {'severity': {'Gte': 3}}}
                 findings = gd.list_findings(DetectorId=d, FindingCriteria=fc)
                 for finding in findings['FindingIds']:
-                    find_detail = gd.get_findings(
-                        DetectorId=d,
-                        FindingIds=[finding],
-                        SortCriteria={
-                            'AttributeName': finding,
-                            'OrderBy': 'DESC'
-                        }
-                    )
-                    print(f'{find_detail}\n')
+                    find_detail = gd.get_findings(DetectorId=d, FindingIds=[finding])
+                    print(f'[*]Access Key:     \t{find_detail["Resource"]["AccessKeyDetails"]["AccessKeyId"]}')
+                    print(f'[*]ID:             \t{find_detail["Resource"]["AccessKeyDetails"]["PrincipalId"]}')
+                    print(f'[*]AWS username:   \t{find_detail["Resource"]["AccessKeyDetails"]["UserName"]}')
 
     except ClientError as e:
         logging.error(e)
