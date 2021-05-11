8933508cf50aa944890c1bd4aa0601bcd37d5598
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-05-11 14:59:12 +0100

    Added KeyError fix to GuardDuty response

--- python/aws_boto_guardduty.py
+++ python/aws_boto_guardduty.py
@@ -1,11 +1,22 @@
 import logging
 import boto3
 from botocore.exceptions import ClientError
-
+from dateutil.parser import parse
 
 # Set environment variables:
 #   AWS_PROFILE=default
 #   AWS_DEFAULT_REGION=......
+
+# AWS Response
+# 1   Dict            ( a Dictionary that contains a Dict and List )
+# 2        \--->Dict['ResponseMetadata']
+# 3        \--->List['Findings']
+# 4            \--->Dict
+
+
+def rm_pretty_date(date_text):
+    cleaned_date = parse(date_text)
+    return f'{cleaned_date:%d-%m-%Y\t%H:%M%p}'
 
 
 def rm_list_guardduty_events():
@@ -18,13 +29,26 @@ def rm_list_guardduty_events():
         if no_of_detectors > 0:
             for d in detectors['DetectorIds']:
                 logging.info(f'Detector ID:\t{d}')
+                # Select Severity
                 fc = {'Criterion': {'severity': {'Gte': 3}}}
+                # get finding IDs, and limited severity to avoid too many IDs
                 findings = gd.list_findings(DetectorId=d, FindingCriteria=fc)
+                # loop through finding IDs to get details of each finding
                 for finding in findings['FindingIds']:
                     find_detail = gd.get_findings(DetectorId=d, FindingIds=[finding])
-                    print(f'[*]Access Key:     \t{find_detail["Resource"]["AccessKeyDetails"]["AccessKeyId"]}')
-                    print(f'[*]ID:             \t{find_detail["Resource"]["AccessKeyDetails"]["PrincipalId"]}')
-                    print(f'[*]AWS username:   \t{find_detail["Resource"]["AccessKeyDetails"]["UserName"]}')
+                    for key, value in find_detail.items():
+                        # Only use the list from the find_detail Dict
+                        if isinstance(value, list):
+                            # 4 get the Findings Dict out of the List
+                            find_details = value[0]
+                            # Handle potential Key Errors as some response parameters are not present
+                            print(f'[*]Date:         {rm_pretty_date(find_details.get("CreatedAt", None))}')
+                            print(f'[*]Description:  {find_details.get("Description", "No description given")}')
+                            print(f'[*]Region:       {find_details.get("Region", "No region found")}')
+                            access_keys = find_details.get('Resource', {}).get('AccessKeyDetails', {})
+                            print(f'[*]Access Key:   {access_keys.get("AccessKeyId", "[!]No access key found")}')
+                            print(f'[*]ID:           {access_keys.get("PrincipalId", "[!]No PrincipalId found")}')
+                            print(f'[*]Username:     {access_keys.get("UserName", "[!]No Username found")}\n')
 
     except ClientError as e:
         logging.error(e)
