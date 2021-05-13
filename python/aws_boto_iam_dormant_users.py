37753b2e62d149ff31f39e320c1e449ab01b8223
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-05-13 14:05:50 +0100

    Added a named tuple to make the code clearer

--- python/aws_boto_iam_dormant_users.py
+++ python/aws_boto_iam_dormant_users.py
@@ -1,12 +1,13 @@
 import logging
+from typing import NamedTuple
 from enum import IntEnum
 import boto3
 from botocore.exceptions import ClientError
 from datetime import datetime
 from progress.bar import Bar
 
-# in a big AWS environment, this can take ~ minute to run with 200-300 IAM users
-# Added a progress bar to avoid excess logging
+# in a big AWS environment, this can take ~1 minute to run with 200-300 IAM users
+# Added a progress bar to avoid extra logging + making to clear that script is still in process
 
 # Set environment variables:
 #   AWS_PROFILE=default
@@ -21,10 +22,28 @@ class DormantRules(IntEnum):
     INACTIVE = 180
 
 
-class IAMUser:
+class DormantResults:
 
-    dormant_user_access_keys = 0
-    never_used_user_access_keys = 0
+    def __init__(self, name: str):
+        self.active_users = 0
+        self.inactive_users = 0
+        self.dormant_users = 0
+        self.never_used_users = 0
+
+    def __repr__(self):
+        return f'active_users:        {self.active_users!r}\t' \
+               f'inactive_users:      {self.inactive_users!r}\t' \
+               f'dormant_users:       {self.dormant_users!r}\t' \
+               f'never_used_users:    {self.never_used_users!r}'
+
+
+class AWSKey(NamedTuple):
+    """A named tuple to make checking for the date of an AWS Access Key clear vs key[0]"""
+    key_id: str
+    last_used_date: datetime
+
+
+class IAMUser:
 
     def __init__(self, name: str):
         self.username = name
@@ -41,12 +60,12 @@ class IAMUser:
         """
         for key in self.keys:
             # handle case where AWS IAM account has a single key with no Used Date
-            if key[1] is None and len(self.keys) == 1:
+            if key.last_used_date is None and len(self.keys) == 1:
                 return "Never used"
             # handle case where a Key has no Used Date but has multiple keys ( a common case, when 1 key is back-up )
-            if key[1] is None and len(self.keys) == 2:
+            if key.last_used_date is None and len(self.keys) == 2:
                 continue
-            days_since_today = datetime.today() - key[1].replace(tzinfo=None)
+            days_since_today = datetime.today() - key.last_used_date.replace(tzinfo=None)
             if days_since_today.days < DormantRules.ACTIVE:
                 return "Active"
             elif days_since_today.days < DormantRules.INACTIVE:
@@ -57,6 +76,10 @@ class IAMUser:
         return f'IAM user:       {self.username!r}\t' \
                f'Key count:      {self.key_count()!r}\t' \
                f'Dormant status: {self.get_dormant_status()!r}'
+
+
+class AccessKey:
+    pass
 
 
 def rm_find_dormant_iam_keys():
@@ -86,32 +109,29 @@ def rm_find_dormant_iam_keys():
                     access_key_id = keys.get("AccessKeyId", {})
                     last_access_date_dict = iam.get_access_key_last_used(AccessKeyId=access_key_id)
                     last_access_date = last_access_date_dict.get('AccessKeyLastUsed', {}).get('LastUsedDate', None)
-                    user.keys.append((access_key_id, last_access_date))
+                    key_to_add = key_tuple(key_id=access_key_id, last_used_date=last_access_date)
+                    user.keys.append(key_to_add)
+
                 iam_users.append(user)
                 bar.next()
         bar.finish()
-        # rm_print_dormant_users(iam_users)
-        logging.info(f'dormant_users:   {IAMUser.dormant_user_access_keys}')
-        logging.info(f'never used:      {IAMUser.never_used_user_access_keys}')
+        rm_print_iam_users(iam_users)
+        logging.info(f'dormant_users:   ')
+        logging.info(f'never used:      ')
 
     except ClientError as e:
         logging.error(e)
         exit()
 
 
-def rm_print_dormant_users(users: list):
+def rm_print_iam_users(users: list):
     from texttable import Texttable
     table = Texttable(max_width=200)
     table.set_cols_width([50, 10, 20])
     table.header(['IAM User', 'Keys', 'Status'])
     table.set_deco(table.BORDER | Texttable.HEADER | Texttable.VLINES | Texttable.HLINES)
     for iam_user in users:
-        if iam_user.get_dormant_status() == "Dormant":
-            IAMUser.dormant_user_access_keys += 1
-            table.add_row([iam_user.username, iam_user.key_count(), iam_user.get_dormant_status()])
-        elif iam_user.get_dormant_status() == "Never used":
-            IAMUser.never_used_user_access_keys += 1
-            table.add_row([iam_user.username, iam_user.key_count(), iam_user.get_dormant_status()])
+        table.add_row([iam_user.username, iam_user.key_count(), iam_user.get_dormant_status()])
     print("\n" + table.draw() + "\n")
 
 
