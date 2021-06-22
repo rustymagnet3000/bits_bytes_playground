fd26827103661b56a1c9cc2d217cfefcafe06dc3
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-06-22 10:01:13 +0100

    Rust - updated Config example

--- rust/Config.rs
+++ rust/Config.rs
@@ -1,28 +1,33 @@
 use std::collections::HashMap;
 use config::{Config, File, FileFormat};
 
-// https://github.com/mehcode/config-rs/blob/master/examples/simple/src/main.rs
-
 fn main() {
 
     let mut config = Config::new();
 
-    // required(true) will cause a Thread Panic, if the file cannot be found
+    // required(true) will cause a Thread Panic, if file cannot be found
     config.merge(File::new("config/Settings", FileFormat::Toml).required(true)).unwrap();
-    println!("[*]Config:  {:#?}", config);
 
-    // This line causes "value used here after move"
-    // println!("{:?}", config.try_into::<HashMap<String, String>>().unwrap());
+    /*
+        The following line will fail if you get an individual item from the Config file.
+        It expects a Map like this:
+            [server]
+            debug = false
+            priority = 32
+            key = "aaaasecretkeyaaaa"
+     */
 
-    let settings = config.try_into::<HashMap<String, String>>().unwrap();
-    if !settings.contains_key("deb2ug") {
-        println!("We've got {} settings but not that one!",
-                 settings.len());
-    }
+    let settings = config.get::<HashMap<String, String>>("server");
 
-    // print all settings
-    for (val, key) in &settings {
-        println!("{}: \"{}\"", val, key);
+    if settings.is_ok(){
+        println!("settings ok");
+        let res = settings.unwrap();
+        if !res.contains_key("debug") {
+            println!("We've got {} settings but not that one!",
+                     res.len());
+        }
+        for (val, key) in &res {
+            println!("{}: \"{}\"", val, key);
+        }
     }
 }
-
