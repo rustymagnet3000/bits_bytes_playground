6ce7291f7039e06c24c223e32011cf1c77c21542
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-06-28 13:17:17 +0100

    Rust - better Hashmap example

--- rust/hashmap.rs
+++ rust/hashmap.rs
@@ -1,55 +1,28 @@
-// Example from: https://doc.rust-lang.org/std/collections/struct.HashMap.html
-
+/*
+    match, remove() and get() value from Hashmap or print no match
+ */
 use std::collections::HashMap;
 
-
 fn main() {
-// Type inference lets us omit an explicit type signature
-// `HashMap<String, String>` in this example
-    let mut book_reviews = HashMap::new();
+    let mut hm = HashMap::new();
+    hm.insert(String::from("Alice"), 77);
+    hm.insert(String::from("Bob"), 66);
+    hm.insert(String::from("Yves"), 66);
 
-    book_reviews.insert(
-        "Adventures of Huckleberry Finn".to_string(),
-        "My favorite book.".to_string(),
-    );
-    book_reviews.insert(
-        "Grimms' Fairy Tales".to_string(),
-        "Masterpiece.".to_string(),
-    );
-    book_reviews.insert(
-        "Pride and Prejudice".to_string(),
-        "Very enjoyable.".to_string(),
-    );
-    book_reviews.insert(
-        "The Adventures of Sherlock Holmes".to_string(),
-        "Eye lyked it alot.".to_string(),
-    );
-
-// Check for a specific one.
-// When collections store owned values (String), they can still be
-// queried using references (&str).
-    if !book_reviews.contains_key("Les Misérables") {
-        println!("We've got {} reviews, but Les Misérables ain't one.",
-                 book_reviews.len());
+    hm.remove(&String::from("Yves"));
+    for(k,v) in &hm {
+        println!("{}:{}", k, v);
     }
 
-// oops, this review has a lot of spelling mistakes, let's delete it.
-    book_reviews.remove("The Adventures of Sherlock Holmes");
-
-// Look up the values associated with some keys.
-    let to_find = ["Pride and Prejudice", "Alice's Adventure in Wonderland"];
-    for &book in &to_find {
-        match book_reviews.get(book) {
-            Some(review) => println!("{}: {}", book, review),
-            None => println!("{} is unreviewed.", book)
-        }
+    let names = ["Bob", "Yves"];
+    for name in &names {
+        check(&hm,name);
     }
+}
 
-// Look up the value for a key (will panic if the key is not found).
-    println!("Review for Jane: {}", book_reviews["Pride and Prejudice"]);
-
-// Iterate over everything.
-    for (book, review) in &book_reviews {
-        println!("{}: \"{}\"", book, review);
+pub fn check (hm: &HashMap<String, i32>, name: &str){
+    match hm.get(&String::from(name)) {
+        Some(&n) => println!("Value: {}", n),
+        _ => println!("[!]no match"),       // if None
     }
 }
