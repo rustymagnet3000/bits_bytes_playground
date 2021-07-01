8e822264a894dedfc4371b009329a9e85b7ee8c9
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-07-01 15:05:57 +0100

    rustlang - bcrypt tidy

--- rust/bcrypt.rs
+++ rust/bcrypt.rs
@@ -15,28 +15,45 @@ hash_with_salt() is when you input the salt
 hash_with_result() returns a more useful Result HashParts so you can get Cost, Salt and Hash
 */
 
-use bcrypt::{hash, hash_with_result, verify};
+use bcrypt::{hash, hash_with_result, hash_with_salt, verify};
 
 fn main() -> std::io::Result<()> {
     let user_pswd: &'static str = "foobar1234";
+    let salt: [u8; 16] = [
+        41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41,
+    ];
 
-    // user A
-    let user_a_digest = hash(user_pswd, 10);
-    println!("User A: {} -> {:#?}", user_pswd, user_a_digest);
-    println!(
-        "User A verify:\n\tPassword:\t{}\n\tverify:  \t{:?}",
-        user_pswd, user_a_digest
-    );
+    // user Alice
+    let user_a_digest = hash_with_salt(user_pswd, 10, &salt);
 
-    // user B
+    match user_a_digest {
+        Ok(v) => {
+            let hash = &v.to_string()[29..60];
+            println!(
+                "User A:\n\tHash:    \t{}\n\tSalt:    \t{:?}\n\tCost:    \t{}\n\tFull:    \t{}",
+                hash,
+                salt,
+                v.get_cost(),
+                v.to_string(),
+            );
+            let verify_result = verify(user_pswd, &v.to_string());
+            println!(
+                "User A verify:\n\tPassword:\t{}\n\tverify:  \t{:?}",
+                user_pswd, verify_result
+            );
+        }
+        Err(e) => println!("error getting hash: {:?}", e),
+    }
+
+    // user Bob
     let user_b_digest = hash_with_result(user_pswd, 10);
     match user_b_digest {
         Ok(v) => {
             let hash = &v.to_string()[29..60];
             println!(
-                "User B:\n\tHash:    \t{}\n\tCost:    \t{}\n\tSalt:    \t{}\n\tFull:    \t{}",
+                "User B:\n\tHash:    \t{}\n\tSalt:    \t{:?}\n\tCost:    \t{}\n\tFull:    \t{}",
                 hash,
-                v.get_salt(),
+                v.get_salt().as_bytes(),
                 v.get_cost(),
                 v.to_string(),
             );
@@ -49,23 +66,42 @@ fn main() -> std::io::Result<()> {
         Err(e) => println!("error getting hash: {:?}", e),
     }
 
+    // user Yves
+    let user_y_digest = hash(user_pswd, 10);
+    println!(
+        "User Y verify:\n\tPassword:\t{}\n\tDigest:\t{:#?}",
+        user_pswd, user_y_digest
+    );
+    let verify_result = verify(user_pswd, &user_y_digest.unwrap());
+    println!("User Y verify:\t{:?}", verify_result);
+
     Ok(())
 }
-/*
 
-User A: foobar1234 -> Ok(
-    "$2b$10$oHcZ0Sg.Ar.LEfcbAWW5mu2LEYDx..Fwan.wbgUP8uswWKHhupEJG",
-)
+/*
+User A:
+    Hash:    	XuCckwAXCWFtjphH5ibTCVZ1O0rkLpG
+    Salt:    	[41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41, 41]
+    Cost:    	10
+    Full:    	$2y$10$IQinIQinIQinIQinIQinIOXuCckwAXCWFtjphH5ibTCVZ1O0rkLpG
 User A verify:
     Password:	foobar1234
-    verify:  	Ok("$2b$10$oHcZ0Sg.Ar.LEfcbAWW5mu2LEYDx..Fwan.wbgUP8uswWKHhupEJG")
+    verify:  	Ok(true)
 User B:
-    Hash:    	e.HTsCV7SLM8WsO4cd.KgLVgPiGWGz6
-    Cost:    	wfVoSBwItHWJ4T0BSe.H9e
-    Salt:    	10
-    Full:    	$2y$10$wfVoSBwItHWJ4T0BSe.H9ee.HTsCV7SLM8WsO4cd.KgLVgPiGWGz6
+    Hash:    	p0yHwGVyOqJG9Qwp86M/DjNLQU5bDaq
+    Salt:    	[104, 111, 117, 88, 52, 70, 74, 90, 118, 66, 102, 52, 85, 119, 80, 74, 66, 81, 85, 87, 81, 46]
+    Cost:    	10
+    Full:    	$2y$10$houX4FJZvBf4UwPJBQUWQ.p0yHwGVyOqJG9Qwp86M/DjNLQU5bDaq
 User B verify:
     Password:	foobar1234
     verify:  	Ok(true)
+User Y verify:
+    Password:	foobar1234
+    Digest:	Ok(
+    "$2b$10$Ws5jYTNFJkofM5VgFCNgCOs2R5qecngGqqHWB1fp.4pJ1U3pYiLoK",
+)
+User Y verify:	Ok(true)
 
-*/
+Process finished with exit code 0
+
+ */
