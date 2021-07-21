705a8fffd730cade50b727c5e5a08007edcca5fd
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2021-07-21 12:43:12 +0100

    Python panda updated casting examples

--- python/pandas_count_of_lists.py
+++ python/pandas_count_of_lists.py
@@ -32,7 +32,92 @@ if __name__ == '__main__':
     print(animal_df.value_counts(normalize=True))
 
     # Only show Animals where count is greater than 3
+    data_banner()
     print(animal_df.value_counts().loc[lambda x: x > 3])
 
     # Sort and op take top three rows
+    data_banner()
     print(animal_df.value_counts(ascending=True).head(3))
+
+    # Print column types
+    animal_df.dtypes
+
+    # Cast epoch string to (positive) integer
+    pd.to_numeric(animal_df['createDate'], downcast='unsigned')
+    0    1626601314001
+    1    1626303600001
+    2    1626303600001
+    3    1626303600001
+    4    1626601314000
+    Name: createDate, dtype: uint64
+
+    # Persist cast of column type
+    items_df['createDate']=items_df['createDate'].astype(int)
+    items_df.dtypes
+    name               object
+    createDate       int64
+
+    # Change Epoch Int value to Date
+    items_df['Created_Date'] = pd.to_datetime(items_df['createDate'])
+
+
+
+
+# ********************
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 15 entries, 0 to 14
+# Data columns (total 1 columns):
+#  #   Column  Non-Null Count  Dtype
+# ---  ------  --------------  -----
+#  0   0       15 non-null     object
+# dtypes: object(1)
+# memory usage: 248.0+ bytes
+# None
+# ********************
+#            0
+# 0     Baboon
+# 1     Spider
+# 2     Spider
+# 3     Baboon
+# 4      Cobra
+# 5     Baboon
+# 6     Baboon
+# 7     Spider
+# 8    Leopard
+# 9    Leopard
+# 10  Elephant
+# 11    Baboon
+# 12    Spider
+# 13   Leopard
+# 14   Leopard
+# ********************
+# Cobra       1
+# Elephant    1
+# Leopard     4
+# Spider      4
+# Baboon      5
+# dtype: int64
+# ********************
+# Baboon      5
+# Cobra       1
+# Elephant    1
+# Leopard     4
+# Spider      4
+# dtype: int64
+# ********************
+# Baboon      0.333333
+# Leopard     0.266667
+# Spider      0.266667
+# Cobra       0.066667
+# Elephant    0.066667
+# dtype: float64
+# ********************
+# Baboon     5
+# Leopard    4
+# Spider     4
+# dtype: int64
+# ********************
+# Cobra       1
+# Elephant    1
+# Leopard     4
+# dtype: int64
